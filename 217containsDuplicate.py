e9e6d1aa72f3ea8b2f870127a5fbc522da7d3410
Natthapong Chantaklad <73196876+Alicisox@users.noreply.github.com>
2024-01-26 10:20:40 +1100

    Update 217containsDuplicate.py
    Make code more readable (change a variable name and change an if else condition, add more test cases.

--- 217containsDuplicate.py
+++ 217containsDuplicate.py
@@ -1,13 +1,15 @@
 class Solution:
     def containsDuplicate(self, nums) -> bool:
-        tmp = set()
+        hashset = set()
         for n in nums:
-            if n not in tmp:
-                tmp.add(n)
-            else:
+            if n in hashset:
                 return True
+            hashset.add(n)
         return False
-
+    
+# Testing
+# test = [1,2,3,1]
+# test = [1,2,3,4]
 test = [1,1,1,3,3,4,3,2,4,2]
 
 print(Solution().containsDuplicate(test))
